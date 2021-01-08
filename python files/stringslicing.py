6ebe3cbd4a44f023da1e23b3b3d930aaba47978d
sukyami <sukyami@gmail.com>
2021-01-08 11:53:21 -0500

    1

---
+++ python files/stringslicing.py
@@ -0,0 +1,17 @@
+name = [
+        'sukhyang', 'Marc','MJ','Sarah','Nicholas',
+        'Ariana','Alejandro','Safiyyah','Qiyu',
+        'Ilya','Mohsin','Adrian','Eric','Nurbu',
+        'Ace','Jiayang','Tashi','Genghis','Karen'
+        ]
+
+print(name[0:10])
+print(name[-2:])
+print(name[-1])
+print(name[2:4:6])
+print(name[0:3])
+print(name[3])
+print(name[5:9])
+print(name[2:10])
+print(name[:3])
+print(name[-3:0])
